5db388b337b64b2e02e264bd7a7b44999ab17de7
Scott Caputo <55561456+scottvcaputo@users.noreply.github.com>
2020-10-25 23:53:36 -0400

    Add files via upload

---
+++ Assets/WizMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class WizMove : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+        
+    }
+}
---
+++ Assets/WizardSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class WizardSpawner : MonoBehaviour
+{
+    public float maxTime = 1;
+    private float timer = 0;
+    public GameObject trap;
+    public float height;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject newtrap = Instantiate(trap);
+        newtrap.transform.position = transform.position + new Vector3(0, UnityEngine.Random.Range(-height, height), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > maxTime)
+        {
+            GameObject newtrap = Instantiate(trap);
+            newtrap.transform.position = transform.position + new Vector3(0, UnityEngine.Random.Range(-height, height), 0);
+            Destroy(newtrap, 15);
+            timer = 0;
+        }
+
+        timer += Time.deltaTime;
+    }
+}
